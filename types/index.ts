6766cb55e50397af3738802a33a4199838bccb31
ngfchl <ngfchl@126.comn>
2025-06-11 13:45:36 +0800

    add. 添加统一返回嘞和站点信息类

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,11 @@ export interface Settings {
     token: string;
 }
 
+export interface SiteInfo {
+    mysite: number
+    website: object | null
+}
+
 export interface Site {
     'id': number
     'name': string
@@ -57,4 +62,63 @@ export interface Downloader {
 export interface Category {
     name: string
     savePath: string
+}
+
+
+/**
+ * 统一的JSON返回格式
+ */
+export /**
+ * 统一的JSON返回格式
+ */
+class CommonResponse<T> {
+    code: number;
+    msg: string | null;
+    data: T | null;
+    succeed: boolean;
+
+    constructor(code: number = 0, data: T | null = null, msg: string = '', succeed: boolean = true) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+        this.succeed = succeed;
+    }
+
+    /**
+     * 创建成功响应
+     * @param data 响应数据
+     * @param msg 响应消息
+     */
+    static success<T>(data: T | null = null, msg: string = ''): CommonResponse<T> {
+        return new CommonResponse<T>(0, data, msg, true);
+    }
+
+    /**
+     * 创建错误响应
+     * @param code 错误码
+     * @param msg 错误消息
+     * @param data 错误数据
+     */
+    static error<T>(code: number = -1, msg: string = '', data: T | null = null): CommonResponse<T> {
+        return new CommonResponse<T>(code, data, msg, false);
+    }
+
+    /**
+     * 转换为对象
+     */
+    toObject(): { code: number; msg: string | null; data: T | null; succeed: boolean } {
+        return {
+            code: this.code,
+            msg: this.msg,
+            data: this.data,
+            succeed: this.succeed,
+        };
+    }
+
+    /**
+     * 转换为JSON字符串
+     */
+    toJson(): string {
+        return JSON.stringify(this.toObject());
+    }
 }
