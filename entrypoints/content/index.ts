fd0434be8b56a4c251f4149b39ab55af4c03824b
ngfchl <ngfchl@126.comn>
2025-06-13 23:26:03 +0800

    update. 降级 antdv 后调用 CSS 样式

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -1,7 +1,7 @@
 import {createApp} from "vue"
 import FloatingWindow from "@/components/FloatingWindow.vue";
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
+import 'ant-design-vue/dist/antd.css';
 import {createPinia} from "pinia";
 
 export default defineContentScript({
@@ -13,15 +13,10 @@ export default defineContentScript({
         // 3. Define your UI
         const ui = await createShadowRootUi(ctx, {
             name: 'harvest-ui',
-            position: 'inline',
+            position: "inline",
             anchor: 'body',
             onMount(container) {
-                // Step 1: 动态注入 CSS 到 Shadow DOM
-                // const link = document.createElement('link');
-                // link.rel = 'stylesheet';
-                // link.href = 'https://cdn.jsdelivr.net/npm/ant-design-vue@3.2.17/dist/antd.min.css';
-                // container.shadowRoot!.appendChild(link);
-                // Define how your UI will be mounted inside the container
+
                 const app = createApp(FloatingWindow);
                 app.use(Antd)
                 app.use(createPinia())
--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -3,6 +3,7 @@ import Antd from 'ant-design-vue';
 import './style.css';
 import App from './App.vue';
 import {createPinia} from "pinia";
+import 'ant-design-vue/dist/antd.css';
 
 const pinia = createPinia()
 const app = createApp(App);
