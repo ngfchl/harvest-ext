21a8017c1bfa5acbbfbed2054e47442dfa0acbf2
ngfchl <ngfchl@126.comn>
2025-06-14 20:54:03 +0800

    add. 封装通用API请求函数

---
+++ hooks/requests.ts
@@ -0,0 +1,41 @@
+import {CommonResponse, Settings} from "@/types";
+
+/**
+ * 通用API请求函数
+ * @param params
+ */
+export const fetchApi = async (params: {
+    setting: Settings,
+    path: string,
+    method: string,
+    data?: any,
+}) => {
+    const {setting, path, method = 'Get', data} = params;
+    try {
+        let url = `${setting.baseUrl}${path}`;
+        const fetchOptions: RequestInit = {
+            method,
+            headers: {
+                "Authorization": `Bearer Monkey.${setting.token}`,
+                "Content-Type": "application/json",
+            },
+        };
+        if (data) {
+            fetchOptions.body = JSON.stringify(data);
+        }
+        const response = await fetch(url, fetchOptions);
+        if (!response.ok) {
+            let msg = `服务器连接失败! status: ${response.status}`;
+            console.log(msg);
+            return CommonResponse.error(-1, msg);
+        }
+
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        let msg = `服务器连接失败！${error}`;
+        console.log(msg);
+        return CommonResponse.error(-1, msg);
+    }
+}
