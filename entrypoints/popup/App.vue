136a900fb5a9a85eff8d4d4a811fe613acfbabd7
ngfchl <ngfchl@126.comn>
2025-06-18 13:51:25 +0800

    update. 优化弹出窗口显示效果

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -37,157 +37,151 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="popup-container">
-    <a-layout class="full-height">
-      <a-layout-header class="header">
-        <a-typography-title :level="3" class="title">收割机</a-typography-title>
-      </a-layout-header>
-      <a-layout-content class="content">
-        <a-space direction="vertical">
-          <a-space size="small">
-            <a-form
-                :style="{ maxWidth: formMaxWidth }"
-                class="form-container"
-                layout="vertical"
-            >
-              <a-form-item>
-                <a-input
-                    v-model:value="setting.baseUrl"
-                    placeholder="Harvest服务器地址"
-                />
-              </a-form-item>
+  <a-layout>
+    <a-layout-header class="header">
+      <a-typography-title :level="3" class="title">收割机</a-typography-title>
+    </a-layout-header>
+    <a-layout-content class="content">
+      <a-space align="center" direction="vertical">
+        <a-space size="small">
+          <a-form
+              :style="{ maxWidth: formMaxWidth }"
+              class="form-container"
+              layout="vertical"
+          >
+            <a-form-item>
+              <a-input
+                  v-model:value="setting.baseUrl"
+                  placeholder="Harvest服务器地址"
+              />
+            </a-form-item>
 
-              <a-form-item>
-                <a-input-password
-                    v-model:value.lazy="setting.token"
-                    autofocus
-                    label="Token"
-                    placeholder="安全Token"
-                />
-              </a-form-item>
-              <a-form-item
-                  :wrapper-col="{ span: 24 }"
-              >
-                <a-input
-                    v-model:value.lazy="setting.imgUrl"
-                    autofocus
-                    label="图片地址"
-                    placeholder="图片地址"
-                    style="width: 100%"
-                />
-              </a-form-item>
-            </a-form>
-          </a-space>
-          <a-space>
-            <a-button block ghost type="primary" @click="testServer">
-              登录鉴权
-            </a-button>
+            <a-form-item>
+              <a-input-password
+                  v-model:value.lazy="setting.token"
+                  autofocus
+                  label="Token"
+                  placeholder="安全Token"
+              />
+            </a-form-item>
+            <a-form-item
+                :wrapper-col="{ span: 24 }"
+            >
+              <a-input
+                  v-model:value.lazy="setting.imgUrl"
+                  autofocus
+                  label="图片地址"
+                  placeholder="图片地址"
+                  style="width: 100%"
+              />
+            </a-form-item>
+          </a-form>
+        </a-space>
+        <a-space>
+          <a-button block ghost type="primary" @click="testServer">
+            登录鉴权
+          </a-button>
+          <a-button
+              :href="setting.baseUrl"
+              danger
+              ghost
+              target="_blank"
+              type="primary"
+          >
+            打开网页
+          </a-button>
+        </a-space>
+        <a-space v-if="canSave">
+          <!--            <a-button-->
+          <!--                block-->
+          <!--                type="primary"-->
+          <!--                @click="saveSettings"-->
+          <!--            >-->
+          <!--              保存配置-->
+          <!--            </a-button>-->
+          <a-popover title="缓存服务器数据">
+            <template #content>
+              <p>从收割机服务器拉取站点配置列表，已有站点列表，下载器列表，缓存到本地，减少交互，提高效率</p>
+            </template>
             <a-button
-                :href="setting.baseUrl"
+                block
+                type="primary"
+                @click="cacheServerData"
+            >
+              更新缓存
+            </a-button>
+          </a-popover>
+          <a-popover title="一键同步Cookie">
+            <template #content>
+              <p>一键同步已添加站点的 Cookie 信息，此功能仅同步 Cookie 和 UserAgent</p>
+            </template>
+            <a-button
+                block
+                type="primary"
+                @click="autoSyncCookie"
+            >
+              一键同步
+            </a-button>
+          </a-popover>
+        </a-space>
+        <a-space v-if="canSave">
+          <a-popover title="导入模式开关">
+            <template #content>
+              <p>
+                打开导入模式时，会显示一键添加按钮，此时可以一键导入未添加的站点
+              </p>
+              <p>
+                站点添加成功后会关掉当前页面，未关掉的站点就是添加失败的，可以手动点击同步数据按钮
+              </p>
+              <p>
+                如果你发现你的个人中心或者控制面板打开后页面自动关闭，请关闭导入模式！
+              </p>
+            </template>
+            <a-button
+                v-if="importMode"
+                block
                 danger
                 ghost
-                target="_blank"
                 type="primary"
+                @click="switchImportMode"
             >
-              打开网页
+              <span>导入模式:开</span>
             </a-button>
-          </a-space>
-          <a-space v-if="canSave">
-            <!--            <a-button-->
-            <!--                block-->
-            <!--                type="primary"-->
-            <!--                @click="saveSettings"-->
-            <!--            >-->
-            <!--              保存配置-->
-            <!--            </a-button>-->
-            <a-popover title="缓存服务器数据">
-              <template #content>
-                <p>从收割机服务器拉取站点配置列表，已有站点列表，下载器列表，缓存到本地，减少交互，提高效率</p>
-              </template>
-              <a-button
-                  block
-                  type="primary"
-                  @click="cacheServerData"
-              >
-                更新缓存
-              </a-button>
-            </a-popover>
-            <a-popover title="一键同步Cookie">
-              <template #content>
-                <p>一键同步已添加站点的 Cookie 信息，此功能仅同步 Cookie 和 UserAgent</p>
-              </template>
-              <a-button
-                  block
-                  type="primary"
-                  @click="autoSyncCookie"
-              >
-                一键同步
-              </a-button>
-            </a-popover>
-          </a-space>
-          <a-space v-if="canSave">
-            <a-popover title="导入模式开关">
-              <template #content>
-                <p>
-                  打开导入模式时，会显示一键添加按钮，此时可以一键导入未添加的站点
-                </p>
-                <p>
-                  站点添加成功后会关掉当前页面，未关掉的站点就是添加失败的，可以手动点击同步数据按钮
-                </p>
-                <p>
-                  如果你发现你的个人中心或者控制面板打开后页面自动关闭，请关闭导入模式！
-                </p>
-              </template>
-              <a-button
-                  v-if="importMode"
-                  block
-                  danger
-                  ghost
-                  type="primary"
-                  @click="switchImportMode"
-              >
-                <span>导入模式:开</span>
-              </a-button>
-              <a-button
-                  v-else
-                  block
-                  type="primary"
-                  @click="switchImportMode"
-              >
-                <span>导入模式:关</span>
-              </a-button>
-            </a-popover>
-            <a-popover title="一键添加站点">
-              <template #content>
-                <p>筛选未添加的站点列表，同时在本地的 Cookie 信息中筛选访问过的站点，抓取 Cookie 和
-                  UserAgent，同步到收割机服务器，然后批量打开</p>
-              </template>
-              <a-button
-                  v-if="importMode"
-                  block
-                  ghost
-                  type="primary"
-                  @click="autoAddSites"
-              >
-                一键添加
-              </a-button>
-            </a-popover>
-
-          </a-space>
+            <a-button
+                v-else
+                block
+                type="primary"
+                @click="switchImportMode"
+            >
+              <span>导入模式:关</span>
+            </a-button>
+          </a-popover>
+          <a-popover title="一键添加站点">
+            <template #content>
+              <p>筛选未添加的站点列表，同时在本地的 Cookie 信息中筛选访问过的站点，抓取 Cookie 和
+                UserAgent，同步到收割机服务器，然后批量打开</p>
+            </template>
+            <a-button
+                v-if="importMode"
+                block
+                ghost
+                type="primary"
+                @click="autoAddSites"
+            >
+              一键添加
+            </a-button>
+          </a-popover>
         </a-space>
-      </a-layout-content>
-    </a-layout>
-  </div>
+      </a-space>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <style scoped>
-.popup-container,
 .ant-layout {
   min-height: 200px; /* 设置最小高度防止内容溢出 */
   min-width: 320px;
   overflow: hidden;
-  width: 100%;
-  height: 100%;
 }
 
 .header {
@@ -215,24 +209,4 @@ onMounted(async () => {
   margin: 0 auto;
 }
 
-/* 响应式调整 */
-@media (max-width: 350px) {
-  .ant-input-group {
-    flex-direction: column;
-  }
-
-  .ant-input-group .ant-btn {
-    width: 100%;
-    margin-top: 8px;
-  }
-}
-
-body {
-  margin: 0 !important;
-  padding: 0 !important;
-  height: 100vh !important;
-  width: 100vw !important;
-  overflow: hidden !important;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-}
 </style>
