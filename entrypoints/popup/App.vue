7b32cc65fca9636fd37b0fa484fb01939bcfd66c
ngfchl <ngfchl@126.comn>
2025-06-22 20:33:47 +0800

    update. 调整 firefox 生产模式下无法打开窗口的 BUG

---
+++ entrypoints/popup/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+onMounted(() => {
+  // 页面加载后自动打开目标页面并关闭自己
+  browser.tabs.create({url: browser.runtime.getURL('/operate.html')});
+  window.close();
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
