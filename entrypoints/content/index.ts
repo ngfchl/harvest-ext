d1b402980beebd495f938b56370013ffc22f4b19
ngfchl <ngfchl@126.comn>
2025-07-17 21:24:36 +0800

    add. 添加新站域名

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -242,7 +242,9 @@ export default defineContentScript({
         'https://13city.org/*',
         // 2025-07-17
         'https://pt.agsvpt.cn/*',
-
+        'https://*.cangbao.ge/*',
+        'https://*.hxpt.org/*',
+        'https://hxpt.org/*',
     ],
     // 2. Set cssInjectionMode
     cssInjectionMode: 'ui',
