e899f2877eab2b10edd7eb6618e7f59c85c2fedd
ngfchl <ngfchl@126.com>
2026-07-11 09:59:59 +0800

    update. 更新版本号 0.3.5

--- wxt.config.ts
+++ wxt.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
         ...(enableKey
             ? {}
             : {key: fs.readFileSync('key.pem', 'utf-8')}),
-        version: '0.3.4',
+        version: '0.3.5',
         permissions: ["storage", "cookies", "activeTab", "scripting", 'contextMenus'],
         host_permissions: [
             "*://*/*",
